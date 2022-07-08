37ce62d3607395099bc0be1dbb06d2e6ae0215c1
AlanHeckinJohnston <alanjohnston800@gmail.com>
2022-07-08 13:49:38 -0400

    credentials and chat id

--- CredentialManager.py
+++ CredentialManager.py
@@ -8,10 +8,16 @@ import os
 class CredentialManager:
 
     token = None
+    chat_id = None
+
+    def getChadId() -> str:
+        if CredentialManager.chat_id == None:
+            CredentialManager.chat_id = os.getenv("CHAT_ID")
+        return CredentialManager.chat_id
 
     def getCredential() -> str:
         if CredentialManager.token == None:
-            token = os.getenv("BOT_TOKEN")
-            if token == None:
+            CredentialManager.token = os.getenv("BOT_TOKEN")
+            if CredentialManager.token == None:
                 exception("NO BOT TOKEN PROVIDED")
-        return token
+        return CredentialManager.token
