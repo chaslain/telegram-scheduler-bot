74463a4157e6be14fc5f0ff6d06836573a8ff36e
Alan <alanjohnston800@gmail.com>
2022-10-08 01:33:45 +0000

    tests

--- test/tests.py
+++ test/tests.py
@@ -6,7 +6,7 @@ class TestWeekdayPicker(unittest.TestCase):
 
     def testWeekday(self):
         weekday = 0
-        fcm = bot.FileCandidateManager.FileCandidateManager("weekday_file_test_data/data")
+        fcm = bot.FileCandidateManager.FileCandidateManager("test/weekday_file_test_data/data")
         file = fcm.getCandidateFileName(weekday)
         assert(os.path.exists(file))
 
